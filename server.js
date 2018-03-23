843ee41157c4164c3e6f9b729cd993dfba1753d4
[R3dact3d] <emmettwb@gmail.com>
2018-03-23 09:44:14 -0600

    I just put the core files in

---
+++ server.js
@@ -0,0 +1,137 @@
+// Dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+var stdin = process.openStdin();
+var server = http.Server(app);
+var io = socketIO(server);
+var port = 80;
+var targetDelay = 500;//ms
+// Set the port, default 80
+if (process.argv[2] == undefined) {
+  port = 80;
+} else {
+var port = process.argv[2]; // Use node server.js <port>
+}
+app.set('port', port);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Callbacks
+stdin.addListener("data", function(d) {
+    // note:  d is an object, and when converted to a string it will
+    // end with a linefeed.  so we (rather crudely) account for that
+    // with toString() and then trim()
+    io.sockets.emit("news", "[SERVER ALERT] : " +
+        d.toString().trim());
+  });
+
+// Starts the server.
+server.listen(port, function() {
+  console.log('Starting server on port '+port);
+});
+
+// Functions
+function assignTargets() {
+  for (player in players) {
+  }
+}
+function shift(array) {
+  console.log('Start');
+  console.log(array);
+  var currentIndex = array.length;
+  var temporaryValue, randomIndex;
+
+  array.unshift(array.pop());
+  console.log('End');
+  console.log(array);
+  return array;
+}
+
+function shuffleTargets() {
+  var arr = [];
+  var data = {};
+  for (var key in players) {
+    arr.push(key);
+  }
+  var i = 0;
+  arr = shift(arr);
+  for (var key in players) {
+    console.log(key);
+    data[String(key)] = arr[i];
+    console.log("DATA:");
+    console.log(data);
+    i++;
+  }
+  io.sockets.emit('targets', data);
+}
+// Add the WebSocket handlers
+var players = {};
+io.on('connection', function(socket) {
+  socket.on('new player', function(data) {
+    io.sockets.emit("news", "Player '"+data.name+"' joined"); //When a player joins
+    players[socket.id] = {
+      name: data.name,
+      target: undefined,
+      hunter: undefined,
+      x: Math.floor(Math.random() * 7) * 100 + 100,
+      y: Math.floor(Math.random() * 5) * 100 + 100
+    };
+    setTimeout(shuffleTargets, targetDelay);
+  });
+  socket.on('old player', function(data) {
+    //io.sockets.emit("message", "Player '"+data.name+"' respawned"); //When a player (re)joins
+    players[socket.id] = {
+      name: data.name,
+      target: undefined,
+      hunter: undefined,
+      x: Math.floor(Math.random() * 7) * 100 + 100,
+      y: Math.floor(Math.random() * 5) * 100 + 100
+    };
+    setTimeout(shuffleTargets, targetDelay);
+  });
+  socket.on('data', function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+    player.x = Math.min(Math.max(parseInt(player.x), 0), 800); // Constrain player x
+    player.y = Math.min(Math.max(parseInt(player.y), 0), 600); // Constrain player y
+  });
+  socket.on('disconnect', function() {
+    if (players[socket.id] == undefined) {
+      console.log('error');
+    }else{
+      io.sockets.emit("alert", "Player '"+players[socket.id].name+"' left")
+      delete players[socket.id];
+      setTimeout(shuffleTargets, targetDelay);
+    }
+  });
+  socket.on('message', function(data) {
+    io.sockets.emit("message", data);
+  });
+  socket.on('kill', function(data) {
+    //console.log(players);
+    io.sockets.emit('killed', data);
+    delete players[data];
+    setTimeout(shuffleTargets, targetDelay);
+  });
+});
+setInterval(function() {
+  io.sockets.emit('state', players);
+}, 1000 / 60);
---
+++ static/game.js
@@ -0,0 +1,172 @@
+// Dependencies
+var socket = io();
+//var Processing = require('processing');
+var messages = document.getElementById('messages');
+var lScreen = document.getElementById('login');
+socket.on('message', function(data) {
+  messages.innerHTML += "<div class='message'>"+data+"</div>";
+});
+socket.on('news', function(data) {
+  messages.innerHTML += "<div class='message news'>"+data+"</div>";
+});
+
+var pData = {
+  name: undefined,
+  target: undefined,
+  hunter: undefined,
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+  x: 0,
+  y: 0
+}
+
+var lastData = {}; // Stores the last data recieved, for the target
+
+document.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      pData.left = true;
+      break;
+    case 87: // W
+      pData.up = true;
+      break;
+    case 68: // D
+      pData.right = true;
+      break;
+    case 83: // S
+      pData.down = true;
+      break;
+    case 32: // Spacebar (to tag)
+      tag();
+      //socket.emit('message', 'test');
+      break
+  }
+});
+document.addEventListener('keyup', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      pData.left = false;
+      break;
+    case 87: // W
+      pData.up = false;
+      break;
+    case 68: // D
+      pData.right = false;
+      break;
+    case 83: // S
+      pData.down = false;
+      break;
+  }
+});
+
+function connect() {
+  document.getElementById('connectBtn').disabled = true;
+  document.getElementById('playername').disabled = true;
+  document.getElementById('reconnect').disabled = true;
+  lScreen.style.display = 'none';
+  pData.name=document.getElementById('playername').value.substring(0, 11);
+  socket.emit('new player', pData);
+  setInterval(function() {
+    socket.emit('data', pData);
+  }, 1000 / 60);
+}
+
+function reconnect() {
+  document.getElementById('connectBtn').disabled = true;
+  document.getElementById('playername').disabled = true;
+  document.getElementById('reconnect').disabled = true;
+  lScreen.style.display = 'none';
+  pData.name=document.getElementById('playername').value.substring(0, 11);
+  socket.emit('old player', pData);
+}
+
+function tag() {
+  console.log(pData.x, pData.y, lastData.x, lastData.y);
+  var a = pData.x - lastData.x;
+  var b = pData.y - lastData.y;
+  var dist = Math.sqrt( a*a + b*b );
+  console.log(dist);
+  if (dist < 50) {
+    toKill = pData.target;
+    pData.target = lastData.target;
+    socket.emit('kill', toKill);
+  }
+}
+
+function send() {
+  msg = document.getElementById('msg');
+  sendBtn = document.getElementById('send');
+  socket.emit('message', '['+pData.name+'] '+msg.value);
+  msg.value = '';
+}
+
+function circle(x, y, radius, context, color) {
+  context.fillStyle = color;
+  context.beginPath();
+  context.arc(x, y, radius, 0, 2 * Math.PI);
+  context.fill();
+}
+
+// Collision detection
+function collides(x, y, r, b, x2, y2, r2, b2) {
+    return !(r <= x2 || x > r2 ||
+             b <= y2 || y > b2);
+}
+
+function boxCollides(pos, size, pos2, size2) {
+    return collides(pos[0], pos[1],
+                    pos[0] + size[0], pos[1] + size[1],
+                    pos2[0], pos2[1],
+                    pos2[0] + size2[0], pos2[1] + size2[1]);
+}
+
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext('2d');
+context.font = "10px consolas";
+socket.on('state', function(players) {
+  if (pData.name != undefined && pData.target != undefined) {
+    pData.x = players[socket.id].x;
+    pData.y = players[socket.id].y;
+    lastData = players[pData.target];
+    console.log(lastData.name);
+  }
+  context.clearRect(0, 0, 800, 600);
+  for (var id in players) {
+    if (id == socket.id) {
+      var player = players[id];
+      context.fillStyle = 'blue';
+      context.beginPath();
+      context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+      context.fill();
+      context.fillText("(You)", player.x-12, player.y-15);
+    }else if (id == pData.target){
+      var player = players[id];
+      context.fillStyle = 'orange';
+      context.beginPath();
+      context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+      context.fill();
+      context.fillText(player.name, player.x-5, player.y-15);
+    }else{
+      var player = players[id];
+      context.fillStyle = 'green';
+      context.beginPath();
+      context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+      context.fill();
+      context.fillText(player.name, player.x-5, player.y-15);
+    }
+  }
+});
+socket.on('targets', function(data) {
+  pData.target = data[socket.id];
+});
+
+socket.on('killed', function(data) {
+  if (data == socket.id) {
+    document.getElementById('reconnect').disabled = false;
+    //socket.disconnect();
+  }
+});
