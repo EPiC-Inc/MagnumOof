454d1974e6e23ad61b6452b4e31a59ca997b03d3
[R3dact3d] <emmettwb@gmail.com>
2018-03-28 12:10:38 -0600

    Update game.js

--- static/game.js
+++ static/game.js
@@ -198,7 +198,7 @@ socket.on('killed', function(data) {
   if (data == socket.id) {
     document.getElementById('reconnect').disabled = false;
     pData.dead = true;
-    console.log(pData.kills);
+    console.log('Kills : '+pData.kills);
     //socket.disconnect();
   }
 });
