93ac61953f747db9880fddbb0b7f686d765d7eec
[R3dact3d] <emmettwb@gmail.com>
2018-03-28 12:08:59 -0600

    Update server.js

--- server.js
+++ server.js
@@ -133,7 +133,9 @@ io.on('connection', function(socket) {
     //console.log(players);
     if (socket.id !== data) {
       io.sockets.emit('killed', data);
-      players[socket.id].kills += 1;
+      if (players[socket.id] !== undefined) {
+        players[socket.id].kills += 1;
+      }
       delete players[data];
       setTimeout(shuffleTargets, targetDelay);
     }
